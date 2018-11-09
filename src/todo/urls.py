20230de8309474319caebc86c04e8b05771d341d
Rajjeet Phull <rajjeet.phull@gmail.com>
2018-11-09 09:48:22 -0500

    Added views and routing - basic app running

---
+++ src/todo/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url, include
+from todo import views
+from rest_framework.routers import DefaultRouter
+
+# Create a router and register our viewsets with it
+router = DefaultRouter(trailing_slash=False)
+router.register(r'todos', views.TodoItemViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+]
