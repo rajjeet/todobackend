d9f6b6bf23bf68fba5110a77fa12da2f3248f313
Rajjeet Phull <rajjeet.phull@gmail.com>
2018-11-09 14:10:48 -0500

    Improved test coverage using django nose, pinocchio, and coverage pip packages

--- src/todobackend/settings/test.py
+++ src/todobackend/settings/test.py
@@ -1,6 +1,24 @@
 from base import *
 import os
 
+# Installed Apps
+INSTALLED_APPS += ('django_nose', )
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner' 
+TEST_OUTPUT_DIR = os.environ.get('TEST_OUTPUT_DIR', '.')
+NOSE_ARGS = [
+    '--verbosity=2',        # verbose output
+    '--nologcapture',       # dont't output log capture
+    '--with-coverage',      # active coverage report
+    '--cover-package=todo', # coverage reports will apply to these packages
+    '--with-spec',          # spec style tests
+    '--spec-color',        
+    '--with-xunit',         # enable xunit plugin
+    '--xunit-file=%s/unittests.xml' % TEST_OUTPUT_DIR,
+    '--cover-xml',          # produce XML coverage info
+    '--cover-xml-file=%s/coverage.xml' % TEST_OUTPUT_DIR,
+]
+
+
 # Database 
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 DATABASES = {
@@ -12,4 +30,4 @@ DATABASES = {
         'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
         'PORT': os.environ.get('MYSQL_PORT', '3306'),
     }
-}
+} 
